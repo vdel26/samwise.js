0f62577d0d87c85b854651e92f38af5d4089a266
Victor Delgado <vdel26@gmail.com>
2015-08-04 10:29:01 +0200

    Add tests
    uses Mocha and expect.js

---
+++ test/data.js
@@ -0,0 +1,30 @@
+window.mockData = {
+  sections: [
+    {
+      title: 'Hosting',
+      section: 'hosting',
+      articles: [
+        { name: 'What are host management services?',                url: 'https://www.airbnb.com/help/article/970?topic=197'},
+        { name: 'How much should I charge for my listing?',          url: 'https://www.airbnb.com/help/article/52?topic=207'},
+        { name: 'How can I make my listing photos look their best?', url: 'https://www.airbnb.com/help/article/746?topic=208'}
+      ]
+    },
+    {
+      title: 'Traveling',
+      section: 'traveling',
+      articles: [
+        { name: 'How do I use my Wish Lists?',              url: 'https://www.airbnb.com/help/article/338?topic=191'},
+        { name: 'How do I know if a listing is available?', url: 'https://www.airbnb.com/help/article/137?topic=191'},
+        { name: 'What is a cancellation policy?',           url: 'https://www.airbnb.com/help/article/149?topic=195'},
+        { name: 'When do I pay for a reservation?',         url: 'https://www.airbnb.com/help/article/92?topic=220'},
+        { name: 'Can I pay with any currency?',             url: 'https://www.airbnb.com/help/article/95?topic=222'},
+        { name: 'What are the service fees?',               url: 'https://www.airbnb.com/help/article/384?topic=220'}
+      ]
+    }
+  ],
+  footer: [
+    { name: 'Help center',  url: 'https://www.airbnb.com/help' },
+    { name: 'Contact',      url: 'https://www.airbnb.com/help/contact_us' }
+  ]
+};
+
---
+++ test/test.urlmode.js
@@ -0,0 +1,49 @@
+describe('Samwise Tests', function () {
+
+  describe('basics', function () {
+    it('samwise global should be available', function () {
+      expect(samwise).to.be.ok();
+    });
+
+    it('should be mounted', function () {
+      expect(samwise.mounted).to.be(true);
+    });
+
+    it('should export a toggle function', function () {
+      expect(samwise.toggle).to.not.equal(undefined);
+    });
+
+    it('should have inserted samwise in the page', function () {
+      var sw = document.querySelector('.sw');
+      expect(sw).to.not.equal(null);
+    });
+  });
+
+  describe('with Samwise open', function () {
+    afterEach(function () {
+      var outerContainer = document.querySelector('.sw-outerContainer');
+      if (outerContainer.classList.contains('is-visible')) {
+        samwise.toggle();
+      }
+    });
+
+    it('should open the widget when clicking the trigger', function () {
+      var button = document.querySelector('#trigger');
+      var outerContainer = document.querySelector('.sw-outerContainer');
+      button.click();
+      expect(outerContainer.classList.contains('is-visible')).to.be(true);
+    });
+
+    it('there should be 3 articles', function () {
+      var articles = document.querySelectorAll('.sw-listElem');
+      expect(articles).to.have.length(3);
+    });
+
+    it('there should be 2 buttons', function () {
+      var buttons = document.querySelectorAll('.sw-button');
+      expect(buttons).to.have.length(2);
+    });
+  });
+
+});
+
